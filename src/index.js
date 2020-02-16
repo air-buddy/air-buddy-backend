a31409bd112796c0e4ae3fc35f847e0b0046a391
David Philipson <david.philipson@gmail.com>
2020-02-15 17:43:40 -0800

    Amadeus query for seats

--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
 const Amadeus = require("amadeus");
+const express = require("express");
+const { AMADEUS_API_KEY, AMADEUS_API_SECRET } = require("../secrets.json");
 
-async function main() {}
+const PORT = 3000;
+
+function main() {
+  express()
+    .get("/seats", async (_, res) => res.json(await getSeats()))
+    .listen(PORT, () => console.log(`Listening on port ${PORT}.`));
+}
+
+async function getSeats() {
+  const amadeus = new Amadeus({
+    clientId: AMADEUS_API_KEY,
+    clientSecret: AMADEUS_API_SECRET
+  });
+
+  try {
+    const offersResponse = await amadeus.shopping.flightOffersSearch.get({
+      originLocationCode: "BOS",
+      destinationLocationCode: "BUF",
+      departureDate: "2020-03-08",
+      adults: "1"
+    });
+    const offer = offersResponse.data[0];
+    // console.log(JSON.stringify(offer, null, 2));
+    const seatmapResponse = await amadeus.client.post(
+      "/v1/shopping/seatmaps",
+      JSON.stringify({ data: [offer] })
+    );
+    return seatmapResponse.data[0];
+  } catch (e) {
+    console.error(e);
+    return;
+  }
+}
 
 main();
